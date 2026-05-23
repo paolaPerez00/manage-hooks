025498366d9bb91d80fe740e6e665c5933afc369
gpere13 <gpere13@bancodebogota.com.co>
2026-05-22 19:40:42 -0500

    Add hook useRef and useEffect

--- src/hooks/useCounter.tsx
+++ src/hooks/useCounter.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
 
-export const useCounter = () => {
-    return (
-        <div>
+export const useCounter = (initialValue: number = 1) => {
 
-        </div>
-    )
+    const [counter, setCounter] = useState(initialValue);
+
+    const increment = () => {
+        setCounter(counter + 1)
+    }
+    const decrement = () => {
+        if (counter <= 1) return;
+        setCounter(counter - 1)
+    }
+
+    return {
+        counter,
+        increment,
+        decrement
+    }
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import './index.css'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
-import { PokemonPage } from './03-examples/PokemonPages'
+// import { PokemonPage } from './03-examples/PokemonPage'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
-    <PokemonPage />
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>,
 )
